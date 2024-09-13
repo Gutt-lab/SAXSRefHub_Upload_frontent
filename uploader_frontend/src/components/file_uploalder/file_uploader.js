2a6b01604480773eda22921e1cd5cd212e76d636
Marie Louise <88967873+MarieAbhag@users.noreply.github.com>
2024-09-13 10:55:03 +0200

    made a button active and edited the names.

--- uploader_frontend/src/components/file_uploalder/file_uploader.js
+++ uploader_frontend/src/components/file_uploalder/file_uploader.js
@@ -4,19 +4,17 @@ import "./file_uploader.css";
 export const FileUploader = ({ onSuccess }) => {
   const [file, setFile] = useState(null);
   const [error, setError] = useState("");
-  const [isFilePicked, setIsFilePicked] = useState(false);
   const [uploadStatus, setUploadStatus] = useState("");
-  const [uploadedFiles, setUploadedFiles] = useState([]);
-  
+
   // Refs for form fields
   const facility = useRef();
-  const beam_line = useRef();
+  const beamLine = useRef();
   const measurementTechnique = useRef();
   const experimentProposalId = useRef();
   const dataSetName = useRef();
   const sampleInformation = useRef();
   const detector = useRef();
-  const sample_detector_distance = useRef();
+  const sampleDetectorDistance = useRef();
   const beamEnergy = useRef();
   const pixelSize = useRef();
   const description = useRef();
@@ -26,7 +24,6 @@ export const FileUploader = ({ onSuccess }) => {
 
     if (!selectedFile) {
       setError("No file selected.");
-      setIsFilePicked(false);
       return;
     }
 
@@ -40,48 +37,49 @@ export const FileUploader = ({ onSuccess }) => {
       setError(
         "Invalid file type. Please upload a PDF, JPG, PNG, or TXT file."
       );
-      setIsFilePicked(false);
       return;
     }
 
     const maxSizeInBytes = 50 * 1024 * 1024; // 50MB
     if (selectedFile.size > maxSizeInBytes) {
       setError("File size exceeds 50MB. Please upload a smaller file.");
-      setIsFilePicked(false);
       return;
     }
 
     setFile(selectedFile);
-    setError("");
-    setUploadStatus("");
-    setIsFilePicked(true);
+    setError(""); // Clear previous errors
+    setUploadStatus(""); // Clear upload status
   };
 
   const onSubmit = async (e) => {
     e.preventDefault();
 
     if (!file) {
-      setError("Please select a file before submitting.");
+      // Display an error message if no file is selected
+      setError("Please upload a file before submitting.");
       return;
     }
 
     const data = new FormData();
     data.append("file", file);
     data.append("facility", facility.current.value);
-    data.append("beam_line", beam_line.current.value);
+    data.append("beam_line", beamLine.current.value);
     data.append("measurement_technique", measurementTechnique.current.value);
     data.append("experiment_proposalId", experimentProposalId.current.value);
     data.append("data_set_name", dataSetName.current.value);
     data.append("sample_information", sampleInformation.current.value);
     data.append("detector", detector.current.value);
-    data.append("sample_detector_distance", sample_detector_distance.current.value);
+    data.append(
+      "sample_detector_distance",
+      sampleDetectorDistance.current.value
+    );
     data.append("beam_energy", beamEnergy.current.value);
     data.append("pixel_size", pixelSize.current.value);
     data.append("description", description.current.value);
 
     try {
-      const SERVER_URL = "http://141.99.126.94:5000/api/upload";
-      //const SERVER_URL = "http://localhost:3002/upload";
+      //const SERVER_URL = "http://141.99.126.94:5000/api/upload";
+      const SERVER_URL = "http://localhost:3002/upload";
 
       const response = await fetch(SERVER_URL, {
         method: "POST",
@@ -95,39 +93,36 @@ export const FileUploader = ({ onSuccess }) => {
 
         // Clear the form data and file input
         setFile(null);
-        setIsFilePicked(false);
         facility.current.value = "";
-        beam_line.current.value = "";
+        beamLine.current.value = "";
         measurementTechnique.current.value = "";
         experimentProposalId.current.value = "";
         dataSetName.current.value = "";
         sampleInformation.current.value = "";
         detector.current.value = "";
-        sample_detector_distance.current.value = "";
+        sampleDetectorDistance.current.value = "";
         beamEnergy.current.value = "";
         pixelSize.current.value = "";
         description.current.value = "";
 
-         // Reset the file input by setting its value to an empty string
-         document.getElementById("upload").value = "";
-
-
-        // Clear the list of uploaded files (if any)
-        setUploadedFiles([]);
+        // Reset the file input by setting its value to an empty string
+        document.getElementById("upload").value = "";
 
         // Optional: Hide the success message after a few seconds
         setTimeout(() => {
           setUploadStatus("");
         }, 3000);
-        
       } else {
-        setError("Failed to upload the file. Server returned an unexpected status.");
+        setError(
+          "Failed to upload the file. Server returned an unexpected status."
+        );
       }
     } catch (err) {
       console.error("Error uploading file: ", err);
       setError("Failed to upload the file. Please try again.");
     }
   };
+
   return (
     <div>
       <form method="post" action="#" id="#" onSubmit={onSubmit}>
@@ -145,13 +140,13 @@ export const FileUploader = ({ onSuccess }) => {
             multiple={false} //to upload only one
           />
         </div>
-  
+
         {/* Error message if any */}
         {error && <p className="error-message">{error}</p>}
-  
+
         {/* Success message if uploaded */}
         {uploadStatus && <p className="upload-status">{uploadStatus}</p>}
-  
+
         <div className="form-container">
           {/* Required Fields Section */}
           <div className="form-column required-fields">
@@ -159,7 +154,7 @@ export const FileUploader = ({ onSuccess }) => {
             <label>Facility</label>
             <input type="text" ref={facility} required />
             <label>Beamline</label>
-            <input type="text" ref={beam_line} required />
+            <input type="text" ref={beamLine} required />
             <label>Measurement Technique</label>
             <input type="text" ref={measurementTechnique} required />
             <label>Experiment Proposal ID</label>
@@ -169,14 +164,14 @@ export const FileUploader = ({ onSuccess }) => {
             <label>Sample Information</label>
             <textarea ref={sampleInformation} required />
           </div>
-  
+
           {/* Optional Fields Section */}
           <div className="form-column optional-fields">
             <h3>Optional Information</h3>
             <label>Detector</label>
             <input type="text" ref={detector} />
             <label>Sample Detector Distance</label>
-            <input type="text" ref={sample_detector_distance} />
+            <input type="text" ref={sampleDetectorDistance} />
             <label>Beam Energy</label>
             <input type="text" ref={beamEnergy} />
             <label>Pixel Size</label>
@@ -185,12 +180,10 @@ export const FileUploader = ({ onSuccess }) => {
             <textarea ref={description} />
           </div>
         </div>
-  
+
         {/* Submit Button */}
-        <button type="submit" disabled={!isFilePicked}>
-          Submit
-        </button>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
-}
+};
